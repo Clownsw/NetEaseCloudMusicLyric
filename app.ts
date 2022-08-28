c7480f33536da09118a6b5037b6bf458a692474a
Smliexx <msmliexx1@gmail.com>
2022-08-28 16:03:09 +0800

    do all the work in house
    Signed-off-by: Smliexx <msmliexx1@gmail.com>

--- app.ts
+++ app.ts
@@ -15,15 +15,26 @@ class HomeResource extends Drash.Resource {
 class LyricResource extends Drash.Resource {
     public paths = ["/lyric"];
 
-    public GET(request: Drash.Request, response: Drash.Response): void {
+    public async GET(request: Drash.Request, response: Drash.Response): Promise<void> {
         const songId = request.queryParam("songId");
         if (songId === undefined) {
             return response.json({
                 error: 'Not Found songId',
             })
         } else {
+            let lyricResp = await fetch("https://music.163.com/weapi/song/lyric?csrf_token=", {
+                method: 'POST',
+                headers: {
+                    'authority': 'music.163.com',
+                    'accept': '*/*',
+                    'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
+                    'content-type': 'application/x-www-form-urlencoded',
+                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+                },
+                body: start(songId),
+            })
             return response.json({
-                param: start(111),
+                lryic: await lyricResp.json(),
             })
         }
     }
